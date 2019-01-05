a13ef83249c07ee6182ccadd5a7e00f302eb8509
OorschotLCL <43795395+OorschotLCL@users.noreply.github.com>
2019-01-05 18:34:03 +0100

    Update LCL.ts

--- LCL.ts
+++ LCL.ts
@@ -4,12 +4,21 @@
  */
 
 
-namespace fun {
+//% color=#0fbc11 icon="\uf121"
+namespace LCL-XL {
     /**
     * Computes the famous Fibonacci number sequence!
     */
     //% block
     export function fib(value: number): number {
         return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    }
+	
+	/**
+    * shows LCL-XL
+    */
+    //% block
+    export function LCL(): void {
+        basic.showString("LCL-XL");
     }
 }
